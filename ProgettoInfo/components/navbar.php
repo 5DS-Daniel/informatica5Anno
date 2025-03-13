e3f4921b48b8d2e03bfdf8be3a66c2c1dadd3c4d
5DS-Daniel <145671872+5DS-Daniel@users.noreply.github.com>
2025-03-13 12:55:05 +0100

    aggiunto bottone per caricare prodotto

--- ProgettoInfo/components/navbar.php
+++ ProgettoInfo/components/navbar.php
@@ -1,8 +1,6 @@
-
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid d-flex justify-content-between align-items-center">
     
-    <!-- Logo e brand -->
     <a class="navbar-brand" href="<?php echo $path2root?>index.php">
       <img src="<?php echo "$path2root"; ?>images/logo.png" alt="" width="30" height="24" class="d-inline-block align-text-top" style="transform: scale(2.5)" >
       Olibrary
@@ -15,29 +13,38 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link" href="<?php echo $path2root?>pages/about.php">About</a>
+          <a class="nav-link" style="font-weight: bold;" href="<?php echo $path2root?>pages/about.php">About</a>
         </li>
 
-
-        <!-- Controllo della sessione -->
         <?php if (isset($_SESSION['user'])): ?>
           <li class="nav-item">
-            <a class="nav-link" href="<?php echo $path2root?>pages/account.php">Account</a>
+            <a class="nav-link" style="font-weight: bold;" href="<?php echo $path2root?>pages/account.php">Account</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="<?php echo $path2root?>pages/logout.php">Logout</a>
+            <a class="nav-link" style="font-weight: bold;" href="<?php echo $path2root?>pages/uploaditem.php">Add item +</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" style="font-weight: bold;" href="<?php echo $path2root?>pages/logout.php">Logout</a>
           </li>
         <?php else: ?>
           <li class="nav-item">
-            <a class="nav-link" href="<?php echo $path2root?>pages/register.php">Register</a>
+            <a class="nav-link" style="font-weight: bold;" href="<?php echo $path2root?>pages/register.php">Register</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="<?php echo $path2root?>pages/login.php">Login</a>
+            <a class="nav-link" style="font-weight: bold;"  href="<?php echo $path2root?>pages/login.php">Login</a>
           </li>
         <?php endif; ?>
-
       </ul>
     </div>
     
   </div>
 </nav>
+
+<!-- Puoi aggiungere il CSS qui sotto o includerlo da un file esterno -->
+<style>
+  .navbar-nav .nav-item .nav-link:hover {
+      background-color: #FFB22C; /* Colore di sfondo quando hover */
+      border-radius: 5px;  /* Per aggiungere angoli arrotondati */
+      transition: background-color 0.3s ease; /* Transizione morbida per l'effetto */
+  }
+</style>
